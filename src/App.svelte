a3b8257bacb8e57339e8056a7f2a794204e4577f
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2024-05-17 02:00:52 +0100

    cesiumjs working

--- src/App.svelte
+++ src/App.svelte
@@ -1,67 +1,11 @@
-
-<!-- Every .svelte file is both a page and component. This makes it so easy to create with it for Single Page Applications. You don't need complicated router logic. You can have the magic just with Modals and Links. Not only!!! But for starters ;) -->
-
-<script lang="ts">
-  
-  // Your Javascript goes here! (you can even copy paste react code here, by only deleting the react boilerplate (usally the lines at the begin) and replace useEffect with onMount. Voila!))
-  
-  // example js begin - delete
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-  // example js end - delete
+<script lang="ts">  
+    
+import Cesium from "./Cesium.svelte";
 
 </script>
 
-  <!-- Your HTML goes here!-->
+<!-- Here starts the app-->
+<div>
+<Cesium/>
 
-  <!-- example html begin - delete -->
-  <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-  <!-- example html end - delete -->
-
-</main>
-
-<!-- Your CSS goes here (inside <style></style>)-->
-
-<!-- example css begin - delete exept <style> -->
-<style>
-
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-
-</style>
-<!-- example css end - delete exept </style> -->
+</div>
--- src/Cesium.svelte
+++ src/Cesium.svelte
@@ -1,30 +1,25 @@
 <script lang="ts">
-// The URL on your server where CesiumJS's static files are hosted.
-window.CESIUM_BASE_URL = '/';
+	import { onMount } from 'svelte';
+	import { Cartesian3, createOsmBuildingsAsync, Ion, Math as CesiumMath, Terrain, Viewer } from 'cesium';
+	import '../node_modules/cesium/Build/Cesium/Widgets/widgets.css'
 
-import { Cartesian3, createOsmBuildingsAsync, Ion, Math as CesiumMath, Terrain, Viewer } from 'cesium';
-import "cesium/Build/Cesium/Widgets/widgets.css";
+  Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkOWYxN2ViMy00ZmM2LTQzNjMtOGZhZC02NDg2ZmY5MDY4MGMiLCJpZCI6MjE1NzcxLCJpYXQiOjE3MTU4OTUxNTd9.29sj9Ay7XCd14X0Z9s-15mMTqcZX1l-PkVo5NHqCKh8';
 
-// Your access token can be found at: https://ion.cesium.com/tokens.
-// Replace `your_access_token` with your Cesium ion access token.
+	window.CESIUM_BASE_URL = './';
 
-Ion.defaultAccessToken = 'your_access_token';
-
-// Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
-const viewer = new Viewer('cesiumContainer', {
-  terrain: Terrain.fromWorldTerrain(),
-});    
-
-// Fly the camera to San Francisco at the given longitude, latitude, and height.
-viewer.camera.flyTo({
-  destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
-  orientation: {
-    heading: CesiumMath.toRadians(0.0),
-    pitch: CesiumMath.toRadians(-15.0),
-  }
-});
-
-// Add Cesium OSM Buildings, a global 3D buildings layer.
-const buildingTileset = await createOsmBuildingsAsync();
-viewer.scene.primitives.add(buildingTileset);   
+    let viewer: Viewer;
+	onMount(async () => {
+		viewer = new Viewer('cesiumContainer');
+	});
+	
 </script>
+
+<div id="cesiumContainer">
+</div>
+
+<style>
+	#cesiumContainer {
+		width: 100%;
+		height: 100vh; /* Setzen Sie die Höhe auf 100% der Ansichtshöhe */
+	}
+</style>
