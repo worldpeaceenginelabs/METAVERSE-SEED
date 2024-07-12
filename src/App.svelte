ba512ccceae8951e31c2491eef407ae37b88bebc
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2024-07-12 12:58:49 +0100

    fix picture jump

--- src/App.svelte
+++ src/App.svelte
@@ -9,41 +9,53 @@
     let quote = "“Decentralization places the globe within your grasp, ensuring your voice resonates daily, not merely at the ballot box every few years. It’s about making your voice count every day, not just delegating it during elections.” BK";
     </script>
     
-    <div>
-        {#if showPicture}
-          <!-- svelte-ignore a11y-click-events-have-key-events -->
-          <!-- svelte-ignore a11y-no-static-element-interactions -->
-          <div class="picture-container" on:click={() => showPicture = false}>
-            <img class="picture" src={pictureUrl} alt="Inspiring" />
-            <div class="quote">{quote}</div>
-          </div>
-        {:else}
-          <Cesium />
-        {/if}
-      </div>
+    <div class="app-container">
+      {#if showPicture}
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <div class="picture-container" on:click={() => showPicture = false}>
+          <img class="picture" src={pictureUrl} alt="Inspiring" />
+          <div class="quote">{quote}</div>
+        </div>
+      {:else}
+        <Cesium />
+      {/if}
+    </div>
 
-    <style>
-      .picture-container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 100vh;
-        width: 100vw;
-        background-color: #f0f0f0;
-        text-align: center;
-      }
-    
-      .picture {
-        max-width: 100%;
-        max-height: 80vh;
-        cursor: pointer;
-      }
-    
-      .quote {
-        margin-top: 20px;
-        font-size: 1.5em;
-        font-style: italic;
-        color: black;
-      }
-    </style>
+      <style>
+        :global(body) {
+          margin: 0;
+          overflow: hidden; /* Prevent scrolling */
+        }
+      
+        .app-container {
+          display: flex;
+          flex-direction: column;
+          height: 100vh;
+          width: 100vw;
+        }
+      
+        .picture-container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          flex: 1; /* Make the container take up the full height */
+          text-align: center;
+          background-color: #f0f0f0;
+        }
+      
+        .picture {
+          max-width: 100%;
+          max-height: 80vh;
+          cursor: pointer;
+        }
+      
+        .quote {
+          margin-top: 20px;
+          font-size: 1.5em;
+          font-style: italic;
+          color: black;
+        }
+      </style>
+      
